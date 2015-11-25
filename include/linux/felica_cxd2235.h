bdb8a31fc8fe6266b36e349dbf839a5a4058667b
android <android>
2015-11-25 17:14:00 +0900

    for b2wlj(b2ul_defconfig) & Multi_ROM

--- include/linux/felica_cxd2235.h
+++ include/linux/felica_cxd2235.h
@@ -53,7 +53,7 @@
  * config option
  ******************************************************************************/
 
-//#define FELICA_CONFIG_ACCESS_RESTRICTION
+#define FELICA_CONFIG_ACCESS_RESTRICTION
 
 struct felica_platform_data {
 	unsigned int int_irq;
